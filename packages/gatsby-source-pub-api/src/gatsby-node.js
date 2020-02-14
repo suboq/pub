d7417f851ea664b96d798e73f7963515d31b16a5
Rodger Jordas <rmjordas@gmail.com>
2020-02-14 22:05:12 +0800

    Remove test api call

--- packages/gatsby-source-pub-api/src/gatsby-node.js
+++ packages/gatsby-source-pub-api/src/gatsby-node.js
@@ -1,20 +1,5 @@
 const { Client } = require('@pub/api-client');
 
-// TODO: Create project schema, pages, paginated projects list, etc.
 exports.sourceNodes = async () => {
-  const client = new Client();
-
-  async function fetch() {
-    const projects = await client.project.getAll();
-
-    if (projects.error) {
-      return projects.error.message;
-    }
-
-    return projects.data;
-  }
-
-  fetch()
-    .then(console.log)
-    .catch(console.error);
+  // TODO: Create project schema, pages, paginated projects list, etc.
 };
